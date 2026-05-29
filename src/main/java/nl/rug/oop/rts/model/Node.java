13202f02dfb871754d8852f22165458f08bfa84e
Harry C. Kepler <qilin2603379191@gmail.com>
2026-05-29 14:10:26 +0200

    feat: add cord for nodes

--- src/main/java/nl/rug/oop/rts/model/Node.java
+++ src/main/java/nl/rug/oop/rts/model/Node.java
@@ -1,22 +1,27 @@
 package nl.rug.oop.rts.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Represents a node (vertex) in the graph with an immutable id and name.
  */
+@Getter
+@Setter
 public class Node {
     /**
      * ID of the node. Add more details here.
      */
-    @Getter
     private final int id;
 
     /**
      * Name of the node. Add more details here.
      */
-    @Getter
     private final String name;
+
+    private int x;
+    private int y;
+
 
     /**
      * Constructor for Node. Add more details here.
