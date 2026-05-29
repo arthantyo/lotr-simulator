d6e6f2ce916e0526863b6be0eb30084adb76af0e
Harry C. Kepler <qilin2603379191@gmail.com>
2026-05-29 14:15:17 +0200

    feat: constrcutor ndoe

--- src/main/java/nl/rug/oop/rts/model/Node.java
+++ src/main/java/nl/rug/oop/rts/model/Node.java
@@ -2,12 +2,14 @@ package nl.rug.oop.rts.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
 
 /**
  * Represents a node (vertex) in the graph with an immutable id and name.
  */
 @Getter
 @Setter
+@AllArgsConstructor
 public class Node {
     /**
      * ID of the node. Add more details here.
@@ -21,16 +23,4 @@ public class Node {
 
     private int x;
     private int y;
-
-
-    /**
-     * Constructor for Node. Add more details here.
-     *
-     * @param id   ID of the node. Must be unique within the graph.
-     * @param name Name of the node. Add more details here.
-     */
-    public Node(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
