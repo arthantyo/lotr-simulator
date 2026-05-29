40047a9967a7e0b9227f038479be86dd0e20f324
A.Koukoumeli <a.koukoumeli@student.rug.nl>
2026-05-29 14:42:55 +0200

    part1.6

--- src/main/java/nl/view/GraphPanel.java
+++ src/main/java/nl/view/GraphPanel.java
@@ -4,14 +4,18 @@ import javax.swing.JPanel;
 import java.awt.Color;
 
 /**
- * Panel where the graph will be drawn.
+ * Panel where the graph will be drawn. Occupies the left (larger) portion
+ * of the main split pane and will be responsible for rendering nodes,
+ * edges, and handling user interactions on the graph canvas.
  */
 public class GraphPanel extends JPanel {
+
     /**
-     * Creates the graph panel with a temporary background color.
+     * Creates the graph panel with a temporary red background.
+     * The red colour is a placeholder to make the panel visible
+     * before actual graph rendering is implemented.
      */
     public GraphPanel() {
         setBackground(Color.RED);
     }
-
 }
