5fabef06c26e4ee18b29e1cb102a612ecc88b214
Harry C. Kepler <qilin2603379191@gmail.com>
2026-05-29 14:33:35 +0200

    fix: view package

--- src/main/java/nl/rug/oop/rts/view/GraphPanel.java
+++ src/main/java/nl/rug/oop/rts/view/GraphPanel.java
@@ -2,16 +2,23 @@ package nl.view;
 
 import javax.swing.JPanel;
 import java.awt.Color;
+import nl.rug.oop.rts.model.*;
 
 /**
  * Panel where the graph will be drawn.
  */
 public class GraphPanel extends JPanel {
+
+    private static final int nodeSize = 40;
+    private final Graph graph;
+
     /**
      * Creates the graph panel with a temporary background color.
      */
     public GraphPanel() {
         setBackground(Color.RED);
     }
+
+
 
 }
