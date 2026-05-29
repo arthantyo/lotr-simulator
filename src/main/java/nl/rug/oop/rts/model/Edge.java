de8a565c62abbb13654c5c0db6302b8d52d98312
arthantyo <tyonirwansyah@gmail.com>
2026-05-29 13:12:50 +0200

    feat: graph listener

--- src/main/java/nl/rug/oop/rts/model/Edge.java
+++ src/main/java/nl/rug/oop/rts/model/Edge.java
@@ -3,21 +3,44 @@ package nl.rug.oop.rts.model;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a directed connection between two {@link Node} instances in the graph.
+ * Contains an immutable id and name and mutable endpoints (`from` and `to`).
+ */
 public class Edge {
+    /**
+     * ID of the edge. Add more details here.
+     */
     @Getter
     private final int id; 
 
+    /**
+     * Name of the edge. Add more details here.
+     */
     @Getter
     private final String name;
 
+    /**
+     * Node from which the edge starts. Add more details here.
+     */
     @Getter
     @Setter
     private Node from;
 
+    /**
+     * Node to which the edge points. Add more details here.
+     */
     @Getter
     @Setter
     private Node to;
 
+    /**
+     * Constructor for Edge. Add more details here.
+     * @param id ID of the edge. Must be unique within the graph.
+     * @param name Name of the edge. Add more details here.
+     * @param from Node from which the edge starts. Must be a node that exists in the graph.
+     * @param to Node to which the edge points. Must be a node that exists in the graph.
+     */
     public Edge(int id, String name, Node from, Node to) {
         this.id = id;
         this.name = name;
